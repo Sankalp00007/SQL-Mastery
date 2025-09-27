7f0ebc8c4f010cd721335783758c8607cac04603
Sankalp00007 <136921400+Sankalp00007@users.noreply.github.com>
2025-09-27 19:47:56 +0530

    Add files via upload

---
+++ Data retrieval/04 order by in sql.sql
@@ -0,0 +1,7 @@
+use employees;
+select * from employees;
+
+select * from employees order by age;
+
+-- arranging in ascending(asc) or descending(desc) order
+select * from employees order by age desc;
