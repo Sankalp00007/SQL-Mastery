e653fcb6a014adafc637fdfa8fe711c8ab8d70b8
Sankalp00007 <136921400+Sankalp00007@users.noreply.github.com>
2025-10-20 21:19:52 +0530

    Add files via upload

---
+++ Introduction to Data Cleaning/handling duplicates.sql
@@ -0,0 +1,23 @@
+use swiggy;
+select * from messy_indian_dataset ;
+
+-- Finding only unique rows
+select distinct * from messy_indian_dataset;
+
+-- Finding unique values based on ID
+select id
+from messy_indian_dataset
+group by id
+order by id;
+
+-- Finding unique values based on name
+select name
+from messy_indian_dataset
+group by name
+order by name;
+
+-- Finding unique values based on id using rank
+select * from (
+					select *, row_number() over(partition by id order by id) as 'rank' from messy_indian_dataset
+               ) as subtable
+where subtable.rank =1;               
